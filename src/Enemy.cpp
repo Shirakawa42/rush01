e0e5e5c2130c599de5ff84afbdc9d8c78f823546
Pierre FICHEPOIL <pfichepo@e2r9p14.42.fr>
2018-06-02 15:54:50 +0200

    updated enemy

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -5,7 +5,14 @@
 Enemy::Enemy(unsigned int x, unsigned int y, unsigned int type) : _x(x), _y(y)
 {
 	// atension ca va aitr moch
-
+	switch (type)
+	{
+		case 1 :
+				this->_size = 1;
+				this->_drawMap = new std::string[1];
+				this->_drawMap[0] = "&";
+				break;
+	}
 
 
 
